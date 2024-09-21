ed6cd966a54f9b55b148a1d7f27c86f13f6a96d0
Philz <dlim@pm-studios.com>
2024-09-21 20:56:40 +0900

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1295777627"
+    serviceWorkerVersion: "827679042"
   }
 });
